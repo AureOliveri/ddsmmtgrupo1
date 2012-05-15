7177b014945d1983df5c482648a562697d486434
Aure.P.Oliveri@gmail.com <Aure.P.Oliveri@gmail.com@5f39e33f-0cc3-4f95-d4bf-f3feb784e0ee>
2012-05-15 02:01:49 +0000

    Arreglo test.

--- trunk/tests/Test/TestAurelio.java
+++ trunk/tests/Test/TestAurelio.java
@@ -1,25 +1,22 @@
 package Test;
 
-import tp.Fecha;
+import org.junit.Assert;
+import org.junit.Test;
+
+import claseUnica.Fecha;
+import claseUnica.parserException;
+
 
 
 public class TestAurelio {
-	
-	public static void main (String [ ] args) {
-		Fecha fecha = new Fecha ("2000-03-31");
-		System.out.println(fecha.getFecha());
-		Fecha otraFecha = new Fecha("01/04/2006");
-		System.out.println(otraFecha.getFecha());
-		Fecha fecha2 = new Fecha ("1991-03-27");
-		System.out.println(fecha2.getFecha());
-		Fecha otraFecha2 = new Fecha("28/03/1991");
-		System.out.println(otraFecha2.getFecha());
-		
-		System.out.println(otraFecha.esAnteriorQue(fecha));
-		System.out.println(otraFecha.cantidadDeDiasEntreFechas(fecha));
-		System.out.println(otraFecha2.esAnteriorQue(fecha2));
-		
-		return;
+	@Test
+	public void tieneFormatoNorteamericano() throws parserException{
+		Fecha fecha1 = new Fecha("03- 29-  1991");
+		Fecha fecha2 = new Fecha("02-29-2012");
+		System.out.println("El formato de fecha 1 es: "+fecha1.getFormato());
+		Assert.assertTrue(fecha1.getFormato().equals("Norteamericano"));
+		System.out.println("El formato de fecha 2 es: "+fecha2.getFormato());
+		Assert.assertTrue(fecha1.getFormato().equals("Norteamericano"));
 	}
 	
 }
