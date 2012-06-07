8959081d51b4a9dd85a835f34c2162925d0832e8
agusr2552@gmail.com <agusr2552@gmail.com@5f39e33f-0cc3-4f95-d4bf-f3feb784e0ee>
2012-06-07 21:58:45 +0000

    Nada, agrego parametros en el test TDD

--- trunk/tests/TestTDD/TDD.java
+++ trunk/tests/TestTDD/TDD.java
@@ -5,6 +5,7 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 import Usuarios.*;
+import Vuelos.Asiento;
 import Vuelos.Vuelo;
 
 public class TDD {
@@ -12,21 +13,26 @@ public class TDD {
 	public void unUsuarioVIPCompraUnoDeLosVuelos(){
 		UsuarioVIP unUsuario = new UsuarioVIP();
 		Vuelo unVuelo = new Vuelo();
+		Asiento unAsiento = new Asiento();
 		unUsuario.compraVuelo(unVuelo);
-		Assert.assertFalse(unVuelo.estaDisponible());
+		Assert.assertFalse(unVuelo.estaDisponible(unAsiento));
 		
 	}
+	@Test
 	public void unUsuarioEstandarCompraUnoDeLosVuelos(){
 		UsuarioEstandar unUsuario = new UsuarioEstandar();
 		Vuelo unVuelo = new Vuelo();
+		Asiento unAsiento = new Asiento();
 		unUsuario.compraVuelo(unVuelo);
-		Assert.assertFalse(unVuelo.estaDisponible());
+		Assert.assertFalse(unVuelo.estaDisponible(unAsiento));
 	}
+	@Test
 	public void unUsuarioNoRegistradoCompraUnoDeLosVuelos(){
 		UsuarioNoRegistrado unUsuario = new UsuarioNoRegistrado();
 		Vuelo unVuelo = new Vuelo();
+		Asiento unAsiento = new Asiento();
 		unUsuario.compraVuelo(unVuelo);
-		Assert.assertFalse(unVuelo.estaDisponible());
+		Assert.assertFalse(unVuelo.estaDisponible(unAsiento));
 	}
 
 
