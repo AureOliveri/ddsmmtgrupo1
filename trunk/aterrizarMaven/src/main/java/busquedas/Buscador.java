ae4527da9fd6a63f1b5c06b75e321813d2788483
agusr2552 <agusr2552@5f39e33f-0cc3-4f95-d4bf-f3feb784e0ee>
2012-11-06 19:44:35 +0000

    Ultima version.

--- trunk/aterrizarMaven/src/main/java/busquedas/Buscador.java
+++ trunk/aterrizarMaven/src/main/java/busquedas/Buscador.java
@@ -28,8 +28,10 @@ public class Buscador {
 		usuario.guardarBusqueda(busqueda);
 		ArrayList<Asiento> asientos = getAerolineas().filtrarAsientos(busqueda);
 		asientos = aplicarFiltros(busqueda, usuario, asientos);
-		
-		return usuario.getAsientosQueLeCorreponden(asientos,
+		for (Vuelo vuelo : getAerolineas().vuelosConEscala(busqueda.getOrigen(), busqueda.getDestino())){
+			asientos.addAll(vuelo.getAsientos());
+			}
+			return usuario.getAsientosQueLeCorreponden(asientos,
 				usuario.getTipoUsuario());
 	}
 
@@ -101,13 +103,5 @@ public class Buscador {
 	public Aerolineas getAerolineas() {
 		return aerolineas;
 	}
-	public ArrayList<Asiento> buscarAsientosConEscala(Busqueda busqueda, Usuario usuario){
-		usuario.guardarBusqueda(busqueda);
-		ArrayList<Asiento> asientos = new ArrayList<Asiento>();		
-		for (Vuelo vuelo : getAerolineas().vuelosConEscala(busqueda.getOrigen(), busqueda.getDestino())){
-			asientos.addAll(vuelo.getAsientos());
-		}
-		return usuario.getAsientosQueLeCorreponden(asientos,
-				usuario.getTipoUsuario());
-	}
+	
 }
